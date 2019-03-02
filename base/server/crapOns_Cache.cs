d07b6c616cc3264687751d25bbca531bf31b73e3
BLID47101 <chazpelo2@gmail.com>
2019-03-02 13:57:32 -0500

    Finally! A baseplate!
    For bedroom in the bin.

--- base/server/crapOns_Cache.cs
+++ base/server/crapOns_Cache.cs
@@ -7,3 +7,6 @@ $CrapOnCRC_["517069892"] = true;
 $CrapOnCRC_["-954107185"] = true;
 $CrapOnCRC_["-865530873"] = true;
 $CrapOnCRC_["517069892"] = true;
+$CrapOnCRC_["-954107185"] = true;
+$CrapOnCRC_["-865530873"] = true;
+$CrapOnCRC_["517069892"] = true;
